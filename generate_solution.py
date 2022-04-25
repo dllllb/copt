43a9fa56f00039255d84166e0bb190e8067c8b1a
sklef <d.s.smolyakov@gmail.com>
2022-04-25 22:15:59 +0300

    Add printing solution in generation process

--- generate_solution.py
+++ generate_solution.py
@@ -142,9 +142,38 @@ def save_data(solution, solution_location):
         pkl.dump(solution, data_file)
 
 
+def print_solution(data, manager, routing, solution):
+    print(f'Objective: {solution.ObjectiveValue()}')
+    total_distance = 0
+    total_load = 0
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        route_distance = 0
+        route_load = 0
+        while not routing.IsEnd(index):
+            node_index = manager.IndexToNode(index)
+            #route_load += data['demands'][node_index]
+            plan_output += ' {0} Load({1}) -> '.format(node_index, route_load)
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id)
+        plan_output += ' {0} Load({1})\n'.format(manager.IndexToNode(index),
+                                                 route_load)
+        plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+        plan_output += 'Load of the route: {}\n'.format(route_load)
+        print(plan_output)
+        total_distance += route_distance
+        total_load += route_load
+    print('Total distance of all routes: {}m'.format(total_distance))
+    print('Total load of all routes: {}'.format(total_load))
+
+
 def main():
     data = load_data(KITCHEN_DATA_LOCATION, TIME_GRAPH_LOCATION)
     routing, solution, manager = solve(data)
+    print_solution(data, manager, routing, solution)
     solution = extract_solution(routing, solution, manager)
     save_data(solution, SOLUTION_LOCATION)
 
