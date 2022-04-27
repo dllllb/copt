1d208b6d55d895c5cd4906e3b22d7d882c9fbc8e
sklef <d.s.smolyakov@gmail.com>
2022-04-27 07:50:28 +0300

    Allroutes in one graph and time of delivery

--- plot_solution.py
+++ plot_solution.py
@@ -1,30 +1,21 @@
 import pandas as pd
+from generate_solution import TIME_GRAPH_LOCATION
 import osmnx as ox
 import pandas as pd
 import pickle as pkl
 import folium
+import datetime
 
 GRAPH_LOCATION = "data/full_graph.pkl"
 PATHS_FILE_LOCATION = "data/paths.pkl"
 SOLUTION_LOCATION = "data/solution.pkl"
 KITCHEN_INFO = "data/kitchens-locations.csv"
-
-def load_graph(graph_location):
-    with open(graph_location, "rb") as data_file:
-        graph = pkl.load(data_file)
-    return graph
+TIME_GRAPH_LOCATION = "data/time.pkl"
 
 
-def read_solution(solution_location):
-    with open(solution_location, 'rb') as data_file:
-        solution = pkl.load(data_file)
-    return solution
-
-
-def load_routes(routes_locations):
-    with open(routes_locations, 'rb') as data_file:
-        routs = pkl.load(data_file)
-    return routs
+def load_pickle_object(path):
+    with open(path, 'rb') as data_file:
+        return pkl.load(data_file)
 
 
 def plot_point(x, y, color, title, map):
@@ -34,41 +25,59 @@ def plot_point(x, y, color, title, map):
                         tooltip=title,
                         color=color).add_to(map)
 
-def plot_solution(graph, all_routes, solution, titles, map_path):
+def plot_route(graph, all_routes, solution, titles, feature_group, color, times):
+    current_time = datetime.datetime(year=2021, month=1, day=1, hour=21)
     route = all_routes[(solution[0], solution[1])]
     start_point = graph.nodes[route[0]]
-    map_ = ox.plot_route_folium(graph, route)
+    ox.plot_route_folium(graph, route, route_map=feature_group)
     folium.CircleMarker(location=[start_point['y'], start_point['x']],
                             radius=5,
                             weight=5,
                             tooltip=titles[solution[0]],
-                            color='red').add_to(map_)
+                            color='red').add_to(feature_group)
     point = graph.nodes[route[-1]]
     folium.CircleMarker(location=[point['y'], point['x']],
                             radius=5,
                             weight=5,
                             tooltip=titles[solution[1]],
-                            color='blue').add_to(map_)
+                            color=color).add_to(feature_group)
     for index in range(1, len(solution)):
         route = all_routes[(solution[index-1], solution[index])]
-        map_ = ox.plot_route_folium(graph, route, route_map=map_)
+        travel_time = times[solution[index-1]][solution[index]]
+        current_time += datetime.timedelta(seconds=travel_time)
+        if index != 1:
+            current_time += datetime.timedelta(minutes=15)
+        ox.plot_route_folium(graph, route, route_map=feature_group, color=color)
         point = graph.nodes[route[-1]]
         folium.CircleMarker(location=[point['y'], point['x']],
                             radius=5,
                             weight=5,
-                            tooltip=titles[solution[index]],
-                            color='blue'
-            ).add_to(map_)
-    map_.save(map_path)
+                            tooltip=f"{titles[solution[index]]} {current_time.strftime('%H:%M:%S')}",
+                            color=color
+            ).add_to(feature_group)
+    return feature_group
+
+def plot_solutions(graph, all_routes, solutions, titles, times):
+    route_map = folium.Map(location=[55.751244, 37.618423])
+    colors = ['blue', 'green', 'purple', 'orange', 'darkred', 'lightred', 'darkblue', 'darkgreen', 'cadetblue', 'darkpurple', 'white', 'pink', 'lightblue', 'lightgreen', 'gray', 'black', 'lightgray'] 
+    for index, elem in enumerate(solutions):
+        feature_group = folium.FeatureGroup(name=f'Track {index}',overlay=True)
+        plot_route(graph, all_routes, elem, titles, feature_group, colors[index % len(colors)], times)
+        feature_group.add_to(route_map)
+        route_map.add_child(feature_group)
+    folium.LayerControl().add_to(route_map)
+    return route_map
+
 
 
 def main():
-    graph = load_graph(GRAPH_LOCATION)
-    all_routes = load_routes(PATHS_FILE_LOCATION)
-    solution = read_solution(SOLUTION_LOCATION)
+    graph = load_pickle_object(GRAPH_LOCATION)
+    all_routes = load_pickle_object(PATHS_FILE_LOCATION)
+    solution = load_pickle_object(SOLUTION_LOCATION)
+    time_graph = load_pickle_object(TIME_GRAPH_LOCATION)
     names = pd.read_csv(KITCHEN_INFO).title.to_list()
-    for index, elem in enumerate(solution):
-        plot_solution(graph, all_routes, elem, names, f"data/route_{index}.html")
+    route_map = plot_solutions(graph, all_routes, solution, names, time_graph)
+    route_map.save("data/routes.html")
 
 
 if __name__ == '__main__':
